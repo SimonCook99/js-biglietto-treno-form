6afde5909d83f77a3981136afba7a4765060464e
Simone Mauro <simon.mauro@virgilio.it>
2022-01-24 18:28:57 +0100

    Esercizio completato

--- main.js
+++ main.js
@@ -7,45 +7,68 @@ bottoneBiglietto.addEventListener("click", function(){
     let kilometri = document.getElementById("kilometri").value;
     let FasciaEta = document.getElementById("Fascia-eta").value;
 
-    let tipologiaTicket = document.getElementById("ticket-type");
-    let carrozza = document.getElementById("carrozza");
-    let CPcode = document.getElementById("CP-code");
-    let prezzo = document.getElementById("price");
 
-
-    let prezzoPieno = 0.21 * kilometri;
-
-    console.log(prezzoPieno);
-
-
-
-    let ticket = document.querySelector(".ticket-container");
-    
-    ticket.classList.remove("hide");
-
-    document.getElementById("name").innerHTML = nomePasseggero;
-
-
-    if(FasciaEta == "Minorenne"){
-        let prezzoSconto20 = prezzoPieno - (prezzoPieno / 100 * 20);
-
-        tipologiaTicket.innerHTML = "Biglietto sconto minorenne";
-        prezzo.innerHTML = prezzoSconto20;
-
-    }else if(FasciaEta == "Boomer ( > 65)"){
-        let prezzoSconto40 = prezzoPieno - (prezzoPieno / 100 * 40);
-
-        tipologiaTicket.innerHTML = "Biglietto sconto boomer";
-        prezzo.innerHTML = prezzoSconto40;
-        /* console.log(prezzoSconto40.toFixed(2)); */
+    /* controllo se i parametri inseriti sono validi, se non lo sono non vado avanti*/
+    if(nomePasseggero == "" || kilometri == "" || FasciaEta == ""){
+        alert("Inserisci dei parametri validi");
     }else{
-        tipologiaTicket.innerHTML = "Biglietto standard";
-        prezzo.innerHTML = prezzoPieno;
+
+        let tipologiaTicket = document.getElementById("ticket-type");
+        let carrozza = document.getElementById("carrozza");
+        let CPcode = document.getElementById("CP-code");
+        let prezzo = document.getElementById("price");
+
+
+
+        let prezzoPieno = 0.21 * kilometri;
+
+        /* prendo il ticket dal DOM e tolgo la classe che lo tiene nascosto*/
+        let ticket = document.querySelector(".ticket-container");
+        ticket.classList.remove("hide");
+
+
+        document.getElementById("name").innerHTML = nomePasseggero;
+
+
+        if(FasciaEta == "Minorenne"){
+            let prezzoSconto20 = prezzoPieno - (prezzoPieno / 100 * 20);
+
+
+            tipologiaTicket.innerHTML = "Biglietto sconto minorenne";
+            prezzo.innerHTML = prezzoSconto20.toFixed(2);
+
+        }else if(FasciaEta == "Boomer"){
+            let prezzoSconto40 = prezzoPieno - (prezzoPieno / 100 * 40);
+
+            tipologiaTicket.innerHTML = "Biglietto sconto boomer";
+            prezzo.innerHTML = prezzoSconto40.toFixed(2);
+        }else{
+            tipologiaTicket.innerHTML = "Biglietto standard";
+            prezzo.innerHTML = prezzoPieno;
+        }
+
+
+        /* numero random da 1 a 9*/
+        carrozza.innerHTML = Math.floor(Math.random() * 10);
+
+        /* numero random da 90000 a 99999*/
+        CPcode.innerHTML = Math.floor(Math.random() * 9000) + 90000;
     }
 
+});
 
 
+let bottoneAnnulla = document.getElementById("Annulla");
 
+bottoneAnnulla.addEventListener("click", function(){
 
-    
+    let nomePasseggero = document.getElementById("input-name").value;
+    let kilometri = document.getElementById("kilometri").value;
+    let FasciaEta = document.getElementById("Fascia-eta").value;
+
+    console.log(nomePasseggero);
+
+    nomePasseggero = kilometri = "";
+    console.log("cliccato");
+    console.log(nomePasseggero);
 });
